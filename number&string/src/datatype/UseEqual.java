a9cf8c2103e01bda39b23d193540a5479ab7a0a1
Ji-Ha <xodhkd36@gmail.com>
2020-11-22 23:05:09 +0900

    equal

---
+++ number&string/src/datatype/UseEqual.java
@@ -0,0 +1,10 @@
+package datatype;
+
+public class UseEqual {
+    public static void main(String[] args) {
+        String a = "Hello World";
+        String b = new String("Hello World");
+        System.out.println(a == b); // 동일한 "객체"가 아니기 때문에 false!
+        System.out.println(a.equals(b)); // 두개의 값은 일치하기 때문에 true.
+    }
+}
