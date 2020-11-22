3b26e7fb46f41fa71b56a4dd0b900d15ef14bdea
Ji-Ha <xodhkd36@gmail.com>
2020-11-22 23:22:50 +0900

    statements

---
+++ number&string/src/statements/if_statement.java
@@ -0,0 +1,19 @@
+package statements;
+
+import java.util.Scanner;
+
+public class if_statement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("누구야 너!");
+        name = sc.next();
+
+        if(name.equals("노지환")){
+            System.out.println("로그인.");
+        }
+        else{
+            System.out.println("추방");
+        }
+    }
+}
