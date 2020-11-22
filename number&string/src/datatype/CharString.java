493e1006af9dbaf7daa2eb44bb25ca7effc489d2
Ji-Ha <xodhkd36@gmail.com>
2020-11-22 22:10:50 +0900

    data type

---
+++ number&string/src/datatype/CharString.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class CharString {
+    public static void main(String[] args) {
+
+        System.out.println("노지환");
+        System.out.println("더하기도" + " 가능하네");
+
+        //따옴표들을 출력하고 싶다면?
+        //System.out.println(""이런다고 안돼요"");
+
+        System.out.println("준비됐어? \'노\'");
+    }
+}
